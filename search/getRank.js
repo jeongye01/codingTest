325d1a052cbe1d2d934fb70ea6e066438a70f2dc
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-05-01 16:50:57 +0900

    등수 구하기

--- search/getRank.js
+++ search/getRank.js
@@ -1,17 +1,12 @@
 //등수 구하기
-function solution(n, arr) {
-  let answer = [];
-  for (let i = 0; i < n; i++) {
-    let order = 1;
-    for (let j = 0; j < n; j++) {
-      if (i === j) {
-        continue;
-      }
-      if (arr[i] < arr[j]) order += 1;
-    }
-    answer.push(order);
-  }
-  return answer;
-}
 
+function solution(n, arr) {
+  let ans = Array.from({ length: n }, () => 1);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i] < arr[j]) ans[i] += 1;
+    }
+  }
+  return ans;
+}
 console.log(solution(5, [87, 89, 92, 100, 76]));
