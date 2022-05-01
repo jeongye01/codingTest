661dcea7acd8fce36598426afb198b2bc91531d9
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-05-01 22:03:12 +0900

    숫자만 추출

---
+++ search_string/extractOnlyNumbers.js
@@ -0,0 +1,11 @@
+//숫자만 추출
+function solution(str) {
+  let answer = "";
+  //소문자가 아닌 것들(^)을 ''로 대치
+  answer = +str.replace(/[^0-9]/g, "");
+
+  return answer;
+}
+
+let str = "g0en2T0s8eSoft";
+console.log(solution(str));
