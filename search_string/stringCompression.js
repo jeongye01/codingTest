59e362201c85b47f247e46cded520164c1a37f6e
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-05-02 20:32:41 +0900

    문자열 압축

--- search_string/stringCompression.js
+++ search_string/stringCompression.js
@@ -2,27 +2,20 @@
 
 function solution(str) {
   let answer = "";
-  let cnt = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    //중복 없는 첫 문자
-    if (cnt === 0) {
+  let cnt = 1;
+  str = str + " ";
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === str[i + 1]) cnt++;
+    else {
       answer += str[i];
-      cnt++;
-    } else if (str[i - 1] === str[i]) {
-      //중복
+      if (cnt > 1) answer += cnt;
 
-      cnt++;
-    } else {
-      //중복 끝남 ,cnt 초기화
-      answer += cnt;
-      answer += str[i];
-      cnt = 0;
+      cnt = 1;
     }
   }
 
   return answer;
 }
 
-let str = "KKHSSSSSSSE";
+let str = "KKHSSSSSSSEEE";
 console.log(solution(str));
