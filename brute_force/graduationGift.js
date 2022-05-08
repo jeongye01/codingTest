085f299ad0fa30d3691a819c7cd7f5d334e6dd95
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-05-08 19:52:30 +0900

    졸업 선물

--- brute_force/graduationGift.js
+++ brute_force/graduationGift.js
@@ -6,6 +6,7 @@ function solution(m, products) {
   products.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
 
   for (let i = 0; i < n; i++) {
+    //모든 상품에 대해 쿠폰 할인을 해봐야함=>브루스 포스
     let money = m - (products[i][0] / 2 + products[i][1]);
     let cnt = 1;
     for (let j = 0; j < n; j++) {
